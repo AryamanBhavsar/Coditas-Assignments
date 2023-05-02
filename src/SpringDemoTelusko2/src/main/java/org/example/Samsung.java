60bc7b35ad1c5022139f35c1b1ccd0f94e64adb3
Aryaman Bhavsar <124352135+AryamanBhavsar-coditas@users.noreply.github.com>
2023-05-02 16:18:42 +0530

    Add files via upload

---
+++ src/SpringDemoTelusko2/src/main/java/org/example/Samsung.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Samsung {
+
+    @Autowired
+    MobileProcessor cpu;
+
+    public MobileProcessor getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(MobileProcessor cpu) {
+        this.cpu = cpu;
+    }
+
+    public void config(){
+        System.out.printf("Galaxy S8 plus");
+        cpu.process();
+    }
+}
